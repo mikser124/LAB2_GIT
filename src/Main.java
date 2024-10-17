717b8fd040827f7885b0350253f8e8b3157e0478
Nazar <s97817@pollub.edu.pl>
2024-10-17 13:16:34 +0200

    feat: added new fanction

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hi");
+        System.out.println("Hi2");
+        System.out.println("hi3");
+        System.out.println("blalala");
+        System.out.println("blalala2");
+    }
+}
+//mikser124
